4d05c81c38fb2aa222205e4fa9e8dd1dbdb5fd38
Artem Vol <artemvol1998@gmail.com>
2024-12-16 20:05:18 +0300

    Update auth-microservice. Update .env.example. Update README.

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
-# Authentication FastAPI microservice
-
+# Authentication microservice
+Authentication microservice with system of roles, OAuth, tests.
 ### Built with
-
 * ![Docker][Docker]
 * ![Fastapi][Fastapi]
 * ![PostgreSQL][PostgreSQL]
+* ![Redis][Redis]
+* ![Jaeger][Jaeger]
 * ![Uvicorn][Uvicorn]
 
-
-Functions |
--- |
-`SignUp` |
-`SignIn` |
-`Logout` |
+### Scheme of services
+<image src="readme/openapi.png" alt="OpenAPI"> </image>
 
 
-## Run development server
+### Environment
+Create .env file using .env.example.
+
+### Run
 ```
-uvicorn main:app
+docker compose up --build
+```
+
+### OpenAPI
+```
+http://127.0.0.1:8081/api/openapi
 ```
 
 
+[Redis]: https://img.shields.io/badge/redis-000000?style=for-the-badge&logo=redis&logoColor=red
 [Docker]: https://img.shields.io/badge/docker-000000?style=for-the-badge&logo=docker&logoColor=blue
 [Fastapi]: https://img.shields.io/badge/fastapi-000000?style=for-the-badge&logo=fastapi&logoColor
 [PostgreSQL]: https://img.shields.io/badge/postgresql-000000?style=for-the-badge&logo=postgresql&logoColor=blue
 [Uvicorn]: https://img.shields.io/badge/uvicorn-000000?style=for-the-badge&logo=uvicorn&logoColor=green
+[Jaeger]: https://img.shields.io/badge/jaeger-000000?style=for-the-badge&logo=jaeger&logoColor=blue
---
+++ src/core/config.py
@@ -0,0 +1,69 @@
+import os
+from logging import config as logging_config
+from typing import ClassVar
+
+from pydantic_settings import BaseSettings
+
+from src.core.logger import LOGGING
+
+
+class Settings(BaseSettings):
+    base_dir: ClassVar[str] = os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+    )
+
+    project_name: str = os.getenv("AUTH_PROJECT_NAME", "movies")
+
+    redis_host: str = os.getenv("REDIS_HOST", "redis")
+    redis_port: int = os.getenv("REDIS_PORT", 6379)
+
+    pg_user: str = os.getenv("AUTH_DB_USER", "user_auth")
+    pg_pass: str = os.getenv("AUTH_DB_PASSWORD", "123456")
+    pg_host: str = os.getenv("AUTH_DB_HOST", "localhost")
+    pg_port: int = os.getenv("AUTH_DB_PORT", 5433)
+    pg_db: str = os.getenv("AUTH_DB", "db_auth")
+    pg_engine: str = os.getenv(
+        "POSTGRES_ENGINE", "django.db.backends.postgresql")
+
+    sqlalchemy_echo: bool = os.getenv("SQLALCHEMY_ECHO", False)
+
+    # Google OAuth settings
+    google_client_id: str = os.getenv(
+        "GOOGLE_CLIENT_ID",
+        "953340218392-lrsn9fhias75cjffgoesadva2ejfhk9p.apps.googleusercontent.com",
+    )
+    google_client_secret: str = os.getenv(
+        "GOOGLE_CLIENT_SECRET", "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"
+    )
+    google_metadata_url: str = (
+        "https://accounts.google.com/.well-known/openid-configuration"
+    )
+
+    # Secret key for JWT
+    secret_key: str = os.getenv("SECRET_KEY", "practix")
+
+    limit_of_requests_per_minute: int = os.getenv(
+        "LIMIT_OF_REQUESTS_PER_MINUTE", 20)
+
+    # Jaeger Config
+    enable_tracer: bool = os.getenv("ENABLE_TRACER", False)
+    jaeger_host: str = os.getenv("JAEGER_HOST", "jaeger")
+    jaeger_port: int = os.getenv("JAEGER_PORT", 6831)
+
+    # Middleware settings
+    middleware_secret_key: str = os.getenv(
+        "MIDDLEWARE_SECRET", "super-secret-key")
+
+    @property
+    def dsn(self) -> str:
+        return f"postgresql+asyncpg://{self.pg_user}:{self.pg_pass}@{self.pg_host}:{self.pg_port}/{self.pg_db}"
+
+    @property
+    def redis_url(self) -> str:
+        return f"redis://{self.redis_host}:{self.redis_port}"
+
+
+# Logging settings
+logging_config.dictConfig(LOGGING)
+
+config = Settings()
